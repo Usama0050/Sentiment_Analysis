5c1c884e759d1cb3044234314cd9f375023ac70e
Usama0050 <usamasadique176@gmail.com>
2025-07-28 06:33:26 +0500

    Update movie.py

--- movie.py
+++ movie.py
@@ -51,9 +51,9 @@ MAX_SEQUENCE_LENGTH = 100
 # Begin form
 with st.form("titanic_form"):
     st.title("Sentiment Analyzer")
-    #st.write("Enter a sentence below to predict its sentiment.")
+    st.write("Enter a sentence below to predict its sentiment.")
 
-    review = st.text_area("Enter a sentence below to predict its sentiment.", height=150)
+    review = st.text_area("Moview Review", height=150)
 
     # ✅ Submit button required for forms
     submit = st.form_submit_button("Predict Sentiment")
